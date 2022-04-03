1da3656c92d8849e6af3e84c952c063bd738f834
alinanefedowa <nefedowa.as@gmail.com>
2022-04-03 22:28:31 +0300

    Assertions were added

--- src/test/java/guru/qa/StudentRegistrationFormTests.java
+++ src/test/java/guru/qa/StudentRegistrationFormTests.java
@@ -18,14 +18,21 @@ public class StudentRegistrationFormTests {
     String firstName = "Alisa";
     String lastName = "Berg";
     String email = "alisa.berg@gmail.com";
+    String gender = "Female";
     String mobile = "9213332221";
+    String dateOfBirth = "12 April,1992";
+    String subjects = "Computer science";
+    String hobby1 = "Sports";
+    String hobby2 = "Reading";
+    String picture = "Picture.jpeg";
     String currentAddress = "Operngasse 15, 7";
+    String state = "Haryana";
+    String city = "Karnal";
 
     @BeforeAll
     static void setUp() {
-        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.browserSize = "1436×732";
+        //Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -37,73 +44,49 @@ public class StudentRegistrationFormTests {
         Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';" +
                 "document.querySelector(\"#fixedban\").hidden = 'true'");
 
-        //Filling in the form
-
-        //Fill in the FirstName
         $("#firstName").setValue(firstName);
-        //Fill in the LastName
         $("#lastName").setValue(lastName);
-        //Fill in the Email
         $("#userEmail").setValue(email);
-        //Fill in the Gender
-        $(byText("Female")).click();
-        //Fill in the Mobile
+        $(byText(gender)).click();
         $("#userNumber").setValue(mobile);
-        //Fill in the Date of Birth
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1992");
         $("[aria-label$='April 12th, 1992']").click();
-        //Fill in the Subjects
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
-        //Fill in the Hobbies
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        //Fill in the Picture
-        $("#uploadPicture").uploadFromClasspath("img/Picture.jpeg");
-        //Fill in the Current address
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $(byText(hobby1)).click();
+        $(byText(hobby2)).click();
+        $("#uploadPicture").uploadFromClasspath("img/"+picture);
         $("#currentAddress").setValue(currentAddress);
 
         //Scroll the page
         $("#stateCity-wrapper").scrollIntoView(true);
 
-        //Fill in the State and City
         $("#state").click();
-        $("#stateCity-wrapper").$(byText("Haryana")).click();
-        //$("#state").click();
-        $("#city").$(byText("Karnal")).click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#city").$(byText(city)).click();
+
         //Click "Submit" button
-        //executeJavaScript("document.body.style.zoom='0.75';document.getElementById(\"submit\").click()");
-        //$("#id=submit").click();
-        //$("#id=submit").shouldBe(visible).click();
+        $("#submit").click();
+
+        //Click "Submit" with changed page zoom
+        // executeJavaScript("document.body.style.zoom='0.75';document.getElementById(\"submit\").click()");
 
         //Assertions
 
-        //$("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-       // $(".modal-body").shouldHave(
-        //  text(firstName + " " + lastName),
-        //  text(email),
-        //  text("Female"),
-        //  text(mobile),
-        //  text("12 April,2022"),
-        //  text("Sports,"Reading"),
-        //  text(hobby),
-        //  text(imgPath.substring(4)),
-        //  text(currentAddress),
-        //  text(state + " " + city)
-       // );
-
-
-//        $("[id=output] [id=name]").shouldHave(text(name));
-//        $("[id=output]").$("[id=name]").shouldHave(text(name));
-//        $("[id=name]").shouldHave(text(name));
-//
-//        $("[id=permanentAddress]").shouldHave(text("Permananet Address :Another street 2")); // wrong
-//        $("[id=permanentAddress]", 1).shouldHave(text("Permananet Address :Another street 2"));
-//        $("p[id=permanentAddress]").shouldHave(text("Permananet Address :Another street 2"));
-//
-//        String expectedPermanentAddress = "Another street 2";
-//        String actualPermanentAddress = $("p[id=permanentAddress]").text();
-//        assertEquals(expectedPermanentAddress, actualPermanentAddress);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(
+                text(firstName + " " + lastName),
+                text(email),
+                text(gender),
+                text(mobile),
+                text(dateOfBirth),
+                text(subjects),
+                text(hobby1 + ", " + hobby2),
+                text(picture),
+                text(currentAddress),
+                text(state + " " + city)
+        );
     }
 }
